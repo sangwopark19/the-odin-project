a09703c011eb2c05b8b0925ef22bda5ed78365e4
sangwopark19 <sangwopark19@gmail.com>
2023-10-08 18:49:22 +0900

    getComputerChoice 함수 생성 가위바위보를 랜덤으로 생성하기위해 만들었다.

--- javascript/rock-paper-scissors/script.js
+++ javascript/rock-paper-scissors/script.js
@@ -1,5 +1,23 @@
 // 최소, 최대값의 범위로 난수를 생성하는 함수
 function randomNumber(min = 0, max = 1) {
-	return Math.floor(Math.random() * (max - min + 1) + min)
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
-console.log(randomNumber(1, 3));
+
+function getComputerChoice() {
+	let num = randomNumber(1, 3)
+	let choice = '';
+	switch (num) {
+		case 1:
+			choice = 'Rock' ;
+			break;
+		case 2:
+			choice = 'Paper'
+			break;
+		case 3:
+			choice = 'Scissors'
+			break;
+	}
+	return choice;
+}
+
+console.log(getComputerChoice());
