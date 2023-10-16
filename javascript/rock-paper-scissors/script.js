b93a5dec23a41ce0ae527046d90c4ac804ea9ed0
sangwopark19 <sangwopark19@gmail.com>
2023-10-16 21:45:49 +0900

    js 사용자로부터 입력을 받는작업까지 완료

--- javascript/rock-paper-scissors/script.js
+++ javascript/rock-paper-scissors/script.js
@@ -1,14 +1,15 @@
 // 최소, 최대값의 범위로 난수를 생성하는 함수
-function randomNumber(min = 0, max = 1) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+function randomNumber() {
+	return Math.floor(Math.random() * 3) + 1;
 }
 
+// 컴퓨터 가위바위보 가져오기
 function getComputerChoice() {
-	let num = randomNumber(1, 3)
+	let num = randomNumber();
 	let choice = '';
 	switch (num) {
 		case 1:
-			choice = 'rock' ;
+			choice = 'rock';
 			break;
 		case 2:
 			choice = 'paper'
@@ -19,14 +20,22 @@ function getComputerChoice() {
 	}
 	return choice;
 }
-
-function playGame(playserSelection, computerSelection) {
-	playserSelection = playserSelection.toLowerCase();
-	if (playserSelection === computerSelection) {
-		
-	}
-	console.log(playserSelection);
-	console.log(computerSelection);
+// 첫 글자 대문자로 변경
+function capitalize(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-playGame('ROCK', getComputerChoice());
+// 가위바위보 결과 출력
+function playRound(playserSelection, computerSelection) {
+	playserSelection = playserSelection.toLowerCase();
+	return 'Your Lose! ' + capitalize(computerSelection) + ' beats ' + capitalize(playserSelection)
+}
+
+function game() {
+	for (let i = 0; i < 5; i++) {
+		console.log(playRound(prompt(), getComputerChoice()));
+	}
+}
+
+game();
+
